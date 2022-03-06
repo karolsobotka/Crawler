d889c9fa32d0c241c513b36e18a8eb17cbe93eb9
s20439 <s20439@pjwstk.edu.pl>
2022-03-06 11:07:23 +0100

    Done

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
     class Program
     {
-        static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             var websiteURL = args[0];
             var httpClient = new HttpClient();
-            var response 
+            var response = await httpClient.GetAsync(websiteURL);
+
+           // Console.WriteLine(response);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            //Console.WriteLine(content);
+
+            var regex = new Regex(@"\b[\w-\.]+@([\w-]+\.)+[\w-]{2,4}\b");
+
+            var a = $"Conetent: {content}";
+            var b = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+
+            var matchCollection = regex.Matches(content);
+
+            
+
+           foreach(Match line in regex.Matches(content))
+            {
+                Console.WriteLine($"{line.Index} : {line.Value}");
+            }
         }   
     }
 }
