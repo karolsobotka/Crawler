084866f6eee4453eafc19353f9eff8f8437fd6ff
s20439 <s20439@pjwstk.edu.pl>
2022-03-06 10:40:20 +0100

    Add project files.

---
+++ Crawler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var websiteURL = args[0];
+            var httpClient = new HttpClient();
+            var response 
+        }   
+    }
+}
